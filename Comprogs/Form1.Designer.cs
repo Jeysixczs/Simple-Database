42746d3d0e8c74175ca89b0b8a295d2156985fde
Jeysixczs <johncarloaquino75@gmail.com>
2024-11-24 21:17:03 +0800

    Add project files.

---
+++ Comprogs/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Comprogs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNames = new System.Windows.Forms.TextBox();
+            this.txtGender = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(395, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ADD / UPDATE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 247);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 290);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 334);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Gender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 373);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 16);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Age";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(94, 240);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(162, 22);
+            this.txtID.TabIndex = 3;
+            // 
+            // txtNames
+            // 
+            this.txtNames.Location = new System.Drawing.Point(94, 284);
+            this.txtNames.Name = "txtNames";
+            this.txtNames.Size = new System.Drawing.Size(162, 22);
+            this.txtNames.TabIndex = 3;
+            // 
+            // txtGender
+            // 
+            this.txtGender.Location = new System.Drawing.Point(94, 328);
+            this.txtGender.Name = "txtGender";
+            this.txtGender.Size = new System.Drawing.Size(162, 22);
+            this.txtGender.TabIndex = 3;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(94, 367);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(162, 22);
+            this.txtAge.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(16, 22);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(273, 196);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(322, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(608, 217);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(609, 290);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 45);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "DELETE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtGender);
+            this.Controls.Add(this.txtNames);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Database";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtNames;
+        private System.Windows.Forms.TextBox txtGender;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Comprogs/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Comprogs
+{
+    public partial class Form1 : Form
+    {
+
+        List<Person> vault = new List<Person>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Person
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Gender { get; set; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnCount = 4;
+            dataGridView1.Columns[0].Name = "ID";
+            dataGridView1.Columns[1].Name = "Name";
+            dataGridView1.Columns[2].Name = "Gender";
+            dataGridView1.Columns[3].Name = "Age";
+        }
+
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            object cellValue = dataGridView1.Rows[e.RowIndex].Cells[0].Value;
+            txtID.Text = Convert.ToString(cellValue);
+
+            object cellValue2 = dataGridView1.Rows[e.RowIndex].Cells[1].Value;
+            txtNames.Text = Convert.ToString(cellValue2);
+
+            object cellValue3 = dataGridView1.Rows[e.RowIndex].Cells[2].Value;
+            txtGender.Text = Convert.ToString(cellValue3);
+
+            object cellValue4 = dataGridView1.Rows[e.RowIndex].Cells[3].Value;
+            txtAge.Text = Convert.ToString(cellValue4);
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            int id = Convert.ToInt32(txtID.Text); 
+
+            Person existingPerson = vault.Find(p => p.ID == id);
+
+            if (existingPerson != null)
+            {
+                existingPerson.Name = txtNames.Text;
+                existingPerson.Gender = txtGender.Text; 
+                existingPerson.Age = Convert.ToUInt16(txtAge.Text);
+                listBox1.Items[listBox1.SelectedIndex] = existingPerson.Name;
+            } 
+            else 
+            { 
+                Person newPerson = new Person() 
+                { 
+                    ID = id, 
+                    Name = txtNames.Text,
+                    Gender = txtGender.Text,
+                    Age = Convert.ToUInt16(txtAge.Text) 
+                }; 
+                vault.Add(newPerson); 
+                listBox1.Items.Add(newPerson.Name);
+            } 
+            dataGridView1.Rows.Clear();
+
+            foreach (Person person in vault) 
+            { 
+                dataGridView1.Rows.Add( 
+                    person.ID,
+                    person.Name,
+                    person.Gender,
+                    person.Age); 
+            } 
+            txtID.Text = "";
+            txtNames.Text = ""; 
+            txtGender.Text = ""; 
+            txtAge.Text = "";
+
+            }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            { 
+                string selectedName = listBox1.SelectedItem.ToString(); 
+                Person selectedPerson = vault.Find(p => p.Name == selectedName);
+                if (selectedPerson != null) 
+                { 
+                    txtID.Text = selectedPerson.ID.ToString(); 
+                    txtNames.Text = selectedPerson.Name;
+                    txtGender.Text = selectedPerson.Gender;
+                    txtAge.Text = selectedPerson.Age.ToString();
+                } 
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                string selectedName = listBox1.SelectedItem.ToString();
+                Person personToDelete = vault.Find(p => p.Name == selectedName);
+                if (personToDelete != null)
+                {
+                    vault.Remove(personToDelete);
+                    listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                    dataGridView1.Rows.Clear();
+                    foreach (Person person in vault)
+                    {
+                        dataGridView1.Rows.Add(
+                            person.ID,
+                            person.Name,
+                            person.Gender,
+                            person.Age);
+                    }
+                    txtID.Text = "";
+                    txtNames.Text = "";
+                    txtGender.Text = "";
+                    txtAge.Text = "";
+                }
+            }
+        }
+    }
+}
